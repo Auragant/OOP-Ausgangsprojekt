494d04941a62296bd644e870dbfc3fd9557d12d0
Adrian Belz <adrian.belz@online.de>
2022-10-13 16:55:19 +0200

    Anpassung von Variablen

--- Beispielprojekt/Main.cpp
+++ Beispielprojekt/Main.cpp
@@ -20,7 +20,7 @@ struct Asteorid {
 
 struct Schuss
 {
-    const int SchussBreite = 5;
+    const int SchussBreite = 10;
     const int SchussHoehe = 3;
     const int SchussPos = 1;
     const Gosu::Color SchussFarbe = Gosu::Color::RED;
@@ -28,7 +28,7 @@ struct Schuss
     double SchussY;
     bool existent = true;
 };
-Schuss neuerStachel(double x,double y) {
+Schuss neuerSchuss(double x,double y) {
     Schuss st;
     st.SchussX = x;
     st.SchussY = y;
@@ -37,61 +37,58 @@ Schuss neuerStachel(double x,double y) {
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class GameWindow : public Gosu::Window
-{
-    Gosu::Font exitgame = { 30 };
-    Gosu::Font auswahl_spaceInvaders = {30};        //Font lässt variablen anzeigen  30 ist die größe
-    int spiel_auswahl = 0;
-    double maus_x;
-    double maus_y;
-    Gosu::Image Raumschiff;
-    Gosu::Font anzeige_score_Space = { 30 };
-    Gosu::Font anzeige_restart_Space = { 30 };
-    Gosu::Font anzeige_start_Space = { 30 };
-    const double SpaceBeeX = 50;
-    bool spielstatus = false;
-    bool initial_start;
-    bool gestorben;
-    double  SpaceBeeY =400;
-    int SpaceScore;
-    int rotationSpace = 270;
-    vector<Asteorid> Asteroiden;
-    vector<Schuss> stachel;
-    double SpaceSpeed = 3;
-    bool SpaceSpielen = false;
-    int AstAnzahl = 0;
-    double stachelSpeed = 5;
-    int ScoreNichtZerstoert;
-    double stachelspeed = 4;
-    int stachelzahl;
-    double SpaceSpeedincrease = 1.08;
-    bool Space_Speed_increase = false;
-    int Space_Speed_increase_at = 5;
+{ 
     Gosu::Image SpaceHintergrund;
     Gosu::Image Asteroid;
     Gosu::Image Logo;
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    Gosu::Image Raumschiff;
+    Gosu::Font exitgame = { 30 };                              
+    Gosu::Font auswahl_spaceInvaders = {30};       
+    Gosu::Font anzeige_score_Space = { 30 };
+    Gosu::Font anzeige_restart_Space = { 30 };
+    Gosu::Font anzeige_start_Space = { 30 };
+    int spielAuswahl = 0;
+    double mausX;
+    double mausY;
+    const double RaumschiffX = 50;
+    double  RaumschiffY =400;
+    bool spielstatus = false;
+    bool initial_start;
+    bool gestorben;
+    int SpaceScore;
+    int rotationSpace = 270;
+    vector<Asteorid> Asteroiden;
+    vector<Schuss> schuesse;
+    double SpaceSpeed = 3;
+    bool SpaceSpielen = false;
+    int AsteroidenAnzahl = 0;
+    double schussSpeed = 5;
+    int ScoreNichtZerstoert;
+    double schussspeed = 4;
+    int schussZahl;
+    double SpaceSpeedincrease = 1.08;
+    bool Space_Speed_increase = false;
+    int Space_Speed_increase_at = 5;
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
 
 public:
-
     GameWindow() : Window(1000, 600), Raumschiff("rakete.png"),SpaceHintergrund("weltraum.png"), Asteroid("asteroid2.png"), Logo("logo.png")
     {
         set_caption("Space Invaders");
     }
-
-  ///////////////////////// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void restart_space() {
         Asteroiden.clear(); 
-        stachel.clear();
-        SpaceBeeY = 400;
+        schuesse.clear();
+        RaumschiffY = 400;
         SpaceScore = 0;
         SpaceSpeed = 3;
         gestorben = false;
         SpaceSpielen = true;
-        AstAnzahl = 0;
+        AsteroidenAnzahl = 0;
         ScoreNichtZerstoert = 0;
-        stachelSpeed = 4;
-        stachelzahl = 0;
+        schussSpeed = 4;
+        schussZahl = 0;
 
     }
     void erstelleAsteroid(vector<Asteorid>& AstVect) {
@@ -103,11 +100,11 @@ public:
     }  
     void AstInRange() {                 
         vector<Asteorid> hilfsvector;
-        vector<Schuss> helpStachel;
+        vector<Schuss> hilfSchuss;
         bool raus = false;
         bool AstRaus = false;  
         for (int ast = 0; ast < Asteroiden.size(); ast++) {
-            if (Asteroiden.at(ast).AsteroidX <= SpaceBeeX + 30 && Asteroiden.at(ast).AsteroidY >= SpaceBeeY - 40 && Asteroiden.at(ast).AsteroidY <= SpaceBeeY + 40) {
+            if (Asteroiden.at(ast).AsteroidX <= RaumschiffX + 30 && Asteroiden.at(ast).AsteroidY >= RaumschiffY - 40 && Asteroiden.at(ast).AsteroidY <= RaumschiffY + 40) {
                 gestorben = true;
                 Asteroiden.at(ast).hit = true;
                 break;
@@ -118,15 +115,15 @@ public:
                 Asteroiden.at(ast).hit = true;
             }
             else {
-                for (int st = 0; st < stachel.size(); st++) {
-                    if (stachel.at(st).SchussX >= Asteroiden.at(ast).AsteroidX && stachel.at(st).SchussX <= Asteroiden.at(ast).AsteroidX + Asteroiden.at(ast).AsteroidBreite
-                        && stachel.at(st).SchussY <= (Asteroiden.at(ast).AsteroidY+ Asteroiden.at(ast).AsteroidLaenge + stachel.at(st).SchussHoehe)&& (stachel.at(st).SchussY >= (Asteroiden.at(ast).AsteroidY ))) {
+                for (int st = 0; st < schuesse.size(); st++) {
+                    if (schuesse.at(st).SchussX >= Asteroiden.at(ast).AsteroidX && schuesse.at(st).SchussX <= Asteroiden.at(ast).AsteroidX + Asteroiden.at(ast).AsteroidBreite
+                        && schuesse.at(st).SchussY <= (Asteroiden.at(ast).AsteroidY+ Asteroiden.at(ast).AsteroidLaenge + schuesse.at(st).SchussHoehe)&& (schuesse.at(st).SchussY >= (Asteroiden.at(ast).AsteroidY ))) {
                         SpaceScore++;
                         Asteroiden.at(ast).hit = true;
-                        stachel.at(st).existent = false;
+                        schuesse.at(st).existent = false;
                     }                      
-                    else if (stachel.at(st).SchussX >= 900.0) {
-                        stachel.at(st).existent = false;
+                    else if (schuesse.at(st).SchussX >= 900.0) {
+                        schuesse.at(st).existent = false;
                     }
                     else {
 
@@ -151,29 +148,29 @@ public:
         }
 
         vector<Schuss> helpSt;
-        for (int st = 0; st < stachel.size(); st++) {
-            if (stachel.at(st).existent == false) {
+        for (int st = 0; st < schuesse.size(); st++) {
+            if (schuesse.at(st).existent == false) {
             }
             else {
-                helpSt.push_back(stachel.at(st));
+                helpSt.push_back(schuesse.at(st));
             }
         }
-        stachel.clear();
+        schuesse.clear();
         for (Schuss st : helpSt) {
-            stachel.push_back(st);
+            schuesse.push_back(st);
         }
     }
-    void StachelBewegen() {
-        for (Schuss& st : stachel) {
-            st.SchussX += stachelspeed;
+    void SchussBewegen() {
+        for (Schuss& st : schuesse) {
+            st.SchussX += schussspeed;
         }
     }
     void AsteroidenBewegen() {
         for (Asteorid& ast : Asteroiden) {
             ast.AsteroidX = ast.AsteroidX - SpaceSpeed;
 
-            if ((SpaceBeeX <= (ast.AsteroidX + (ast.AsteroidBreite / 2)) && SpaceBeeX >= (ast.AsteroidX - (ast.AsteroidBreite / 2))) &&
-                (SpaceBeeY <= (ast.AsteroidY - (ast.AsteroidLaenge / 2)) && SpaceBeeY >= (ast.AsteroidY + (ast.AsteroidLaenge / 2))))
+            if ((RaumschiffX <= (ast.AsteroidX + (ast.AsteroidBreite / 2)) && RaumschiffX >= (ast.AsteroidX - (ast.AsteroidBreite / 2))) &&
+                (RaumschiffY <= (ast.AsteroidY - (ast.AsteroidLaenge / 2)) && RaumschiffY >= (ast.AsteroidY + (ast.AsteroidLaenge / 2))))
             {
                 gestorben = true;
                 break;
@@ -184,21 +181,20 @@ public:
  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void draw() override
     {
-        if (spiel_auswahl == 0) {
+        if (spielAuswahl == 0) {
             SpaceHintergrund.draw(0, 0, 0, 1.0, 1.0);
             Logo.draw(40, 0, 1, 1.0, 1.0);
             auswahl_spaceInvaders.draw_text_rel("Spielen", 515, 500, 1, 0.5, 0.5, 1, 1, Gosu::Color::GREEN);                                                     // Menü beim Start des Spiels              
         }
- 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        if (spiel_auswahl == 1) {
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        if (spielAuswahl == 1) {
             SpaceHintergrund.draw(0, 0, 0, 1.0, 1.0);
             string score = to_string(SpaceScore);
             string nothit = to_string(ScoreNichtZerstoert);
             anzeige_score_Space.draw_text_rel("Score: " + score, 0, 0, 2, 0, 0, 1, 1, Gosu::Color::GREEN);
 
             if (SpaceSpielen) {
-                Raumschiff.draw_rot(SpaceBeeX, SpaceBeeY, 2, rotationSpace
+                Raumschiff.draw_rot(RaumschiffX, RaumschiffY, 2, rotationSpace
                     , 0.5, 0.5,
                     0.1, 0.1
                 );
@@ -209,9 +205,9 @@ public:
 
                     }
                 }
-                for (int st = 0; st < stachel.size(); st++) {
-                    if (stachel.at(st).existent) {
-                        graphics().draw_rect(stachel.at(st).SchussX, stachel.at(st).SchussY, stachel.at(st).SchussBreite, stachel.at(st).SchussHoehe, stachel.at(st).SchussFarbe, stachel.at(st).SchussHoehe);
+                for (int st = 0; st < schuesse.size(); st++) {
+                    if (schuesse.at(st).existent) {
+                        graphics().draw_rect(schuesse.at(st).SchussX, schuesse.at(st).SchussY, schuesse.at(st).SchussBreite, schuesse.at(st).SchussHoehe, schuesse.at(st).SchussFarbe, schuesse.at(st).SchussHoehe);
                     }
                 }
             }
@@ -222,68 +218,69 @@ public:
         }
        
     }
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ // 
     // Wird 60x pro Sekunde aufgerufen
     void update() override
     {
-        maus_x = input().mouse_x();            //Position Maus im Menü
-        maus_y = input().mouse_y();
+        mausX = input().mouse_x();            //Position der Maus im Menü
+        mausY = input().mouse_y();
 
-        /// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        if (spiel_auswahl == 0) {
-            if (maus_x >= 430 && maus_x <= 600 && maus_y >= 400 && maus_y <= 600 && input().down(Gosu::Button::MS_LEFT)) {     
-                spiel_auswahl = 1;                                                                                                // Menüauswahl
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        if (spielAuswahl == 0) {
+            if (mausX >= 430 && mausX <= 600 && mausY >= 400 && mausY <= 600 && input().down(Gosu::Button::MS_LEFT)) {     
+                spielAuswahl = 1;                                                                                                // Menüauswahl
                 SpaceSpielen = true;
                 restart_space();
                
             }
         }
-        /// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
        
-        if (spiel_auswahl == 1) {
+        if (spielAuswahl == 1) {
             if (!gestorben) {
                 if (SpaceScore % Space_Speed_increase_at == 0 && Space_Speed_increase) {
                     SpaceSpeed = SpaceSpeed * SpaceSpeedincrease;
                     Space_Speed_increase = false;
                 }
-                AstAnzahl--;
-                stachelzahl++;
+                AsteroidenAnzahl--;
+                schussZahl++;
                 
                 if (input().down(Gosu::Button::KB_UP)) {
-                    SpaceBeeY -= (SpaceSpeed + 2);
+                    RaumschiffY -= (SpaceSpeed + 2);
                 }
                 else if (input().down(Gosu::Button::KB_DOWN)) {
-                    SpaceBeeY += SpaceSpeed + 2;
+                    RaumschiffY += SpaceSpeed + 2;
                 }
-                if (AstAnzahl <= 1) {
-                    AstAnzahl = 30;
+                if (AsteroidenAnzahl <= 1) {
+                    AsteroidenAnzahl = 30;
                     erstelleAsteroid(Asteroiden);
                     
                        
                 }
-                if (input().down(Gosu::Button::KB_SPACE)&& stachelzahl>=20) {
-                    stachel.push_back(neuerStachel(SpaceBeeX, SpaceBeeY));
-                    stachelzahl = 0;
+                if (input().down(Gosu::Button::KB_SPACE)&& schussZahl>=20) {
+                    schuesse.push_back(neuerSchuss(RaumschiffX, RaumschiffY));
+                    schussZahl = 0;
                 }
                 AstInRange(); 
-                StachelBewegen();
+                SchussBewegen();
                 AsteroidenBewegen();
             }
-            else if (maus_x >= 410 && maus_x <= 590 && maus_y >= 270 && maus_y <= 330 && !spielstatus && input().down(Gosu::Button::MS_LEFT) && !initial_start) {    // Restart Knopf wurde betätigt
+            else if (mausX >= 410 && mausX <= 590 && mausY >= 270 && mausY <= 330 && !spielstatus && input().down(Gosu::Button::MS_LEFT) && !initial_start) {    // Restart Knopf wurde betätigt
                 restart_space();  
                 gestorben = false;
                 return;
             }
-            else if (maus_x >= 900 && maus_x <= 1000 && maus_y >= 0 && maus_y <= 100 && input().down(Gosu::Button::MS_LEFT)) {               //Spiel beenden
+            else if (mausX >= 900 && mausX <= 1000 && mausY >= 0 && mausY <= 100 && input().down(Gosu::Button::MS_LEFT)) {               //Spiel beenden
                 exit(3);
             }
-            if (SpaceBeeY > 600 || SpaceBeeY< 0)              // Abfrage, ob man außerhalb den Bildschirms geflogen ist falls ja Game Over
+            if (RaumschiffY > 600 || RaumschiffY< 0)              // Abfrage, ob man außerhalb den Bildschirms geflogen ist falls ja Game Over
                 gestorben = true;
         }
     }
 };
-
-
+/// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
     GameWindow GameWindow;
