8f3c2af3b77ff7a3ee37b40702750876ab24ad67
Adrian Belz <adrian.belz@online.de>
2022-10-14 17:56:56 +0200

    Kommentare hinzugefügt

--- Beispielprojekt/Main.cpp
+++ Beispielprojekt/Main.cpp
@@ -7,11 +7,7 @@
 #include "Schuss.h"
 using namespace std;
 
-
-
-
-// Deklaration der Variablen
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class GameWindow : public Gosu::Window
 { 
     Gosu::Image SpaceHintergrund;
@@ -43,15 +39,14 @@ class GameWindow : public Gosu::Window
     int AsteroidenAnzahl = 0;
     double schussSpeed = 7;
     int schussZahl;
- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 public:
     GameWindow() : Window(1000, 600), Raumschiff("raumschiff1.png"),SpaceHintergrund("weltraum.png"), Asteroid("asteroid2.png"), Logo("logo.png")
     {
         set_caption("Space Invaders");
     }
-    void restart_space() 
+    void restart_game()                                             // Setzt die veränderten Variablen zurück auf ihren Ursprungswert 
     {
         Asteroiden.clear();
         schuesse.clear();
@@ -64,24 +59,64 @@ public:
         ScoreNichtZerstoert = 0;
         schussSpeed = 7;
         schussZahl = 0;
+    } 
+    void loescheObjekte()                                           // löscht die getroffenen Asteroiden und Schüsse
+    {
+        vector<Asteorid> helpAst;
+        for (int ast = 0; ast < Asteroiden.size(); ast++) 
+        {
+            if (Asteroiden.at(ast).hit == true)                     // wenn der Asteroid getroffen wird er nicht im Hilfsvektor zwischengespeichert
+            {                                                           
+            }
+            else                                                    // Nicht getroffene Asteroiden werden im Hilfsvektor zwischengespeichert
+            {
+                helpAst.push_back(Asteroiden.at(ast));              
+            }
+        }
+        Asteroiden.clear();                                         // Alle getroffenen Asteroiden werden gelöscht
+        for (Asteorid ast : helpAst) 
+        {
+            Asteroiden.push_back(ast);                              // Nicht getroffene Asteroiden werden von dem Hilfsvektor in den Asteroidenvektor kopiert
+        }
+
+        vector<Schuss> helpSt;
+        for (int st = 0; st < schuesse.size(); st++) 
+        {
+            if (schuesse.at(st).existent == false)                  // wenn der Schuss einen Asteroid trifft wird er nicht im Hilfsvektor zwischengespeichert
+            {
+            }
+            else                                                    // wenn der Schuss keinen Asteroiden trifft wird er im Hilfsvektor zwischengespeichert
+            {
+                helpSt.push_back(schuesse.at(st));                  
+            }
+        }
+        schuesse.clear();                                           // Alle Schüsse die Asteroiden getroffenen haben werden gelöscht
+        for (Schuss st : helpSt) 
+        {
+            schuesse.push_back(st);                                 // Schüsse die keine Asteroiden getroffen haben werden aus dem HIlfsvektor in den Schussvektor kopiert
+        }
     }
-    void AstInRange() {                 
+    void FunktionenObjekte() 
+    {                 
         vector<Asteorid> hilfsvector;
         vector<Schuss> hilfSchuss;
         bool raus = false;
         bool AstRaus = false;  
-        for (int ast = 0; ast < Asteroiden.size(); ast++) {    
-            if (Asteroiden.at(ast).AsteroidY >= RaumschiffY - 30 && Asteroiden.at(ast).AsteroidY <= 560 && Asteroiden.at(ast).AsteroidX - 30 <= RaumschiffX  && Asteroiden.at(ast).AsteroidX + 90 >= RaumschiffX ) {
+        for (int ast = 0; ast < Asteroiden.size(); ast++) 
+        {    
+            if (Asteroiden.at(ast).AsteroidY >= RaumschiffY - 30 && Asteroiden.at(ast).AsteroidY <= 560 && Asteroiden.at(ast).AsteroidX - 30 <= RaumschiffX  && Asteroiden.at(ast).AsteroidX + 90 >= RaumschiffX ) // Prüft ob ein Asteroid das Raumschiff trifft
+            {
                 getroffen = true;
                 Asteroiden.at(ast).hit = true;
                 break;
             }
-            else if (Asteroiden.at(ast).AsteroidX >= 600) {
+            else if (Asteroiden.at(ast).AsteroidX >= 600)          // Prüft ob der Asteroid aus dem Bildschirm fliegt
+            {
                 ScoreNichtZerstoert++;
                 speedIncrease = true;
                 Asteroiden.at(ast).hit = true;
             }
-            else {
+            else {                                                 // Prüft ob ein Schuss einen Asteroiden getroffen hat
                 for (int st = 0; st < schuesse.size(); st++) {
                     if (schuesse.at(st).SchussX >= Asteroiden.at(ast).AsteroidX && schuesse.at(st).SchussX <= Asteroiden.at(ast).AsteroidX + Asteroiden.at(ast).AsteroidHoehe
                         && schuesse.at(st).SchussY <= (Asteroiden.at(ast).AsteroidY+ Asteroiden.at(ast).AsteroidBreite + schuesse.at(st).SchussHoehe)&& (schuesse.at(st).SchussY >= (Asteroiden.at(ast).AsteroidY ))) {
@@ -89,124 +124,94 @@ public:
                         Asteroiden.at(ast).hit = true;
                         schuesse.at(st).existent = false;
                     }                      
-                    else if (schuesse.at(st).SchussX <= 0) {
+                    else if (schuesse.at(st).SchussX <= 0)         // Prüft ob ein Schuss aus dem Bildschirm fliegt
+                    {
                         schuesse.at(st).existent = false;
                     }
                 }
             }
         }
-        loescheObjekte();
+        loescheObjekte();                                         // löscht getroffene Asteroiden und Schüsse
     }
-    void loescheObjekte() {
-        vector<Asteorid> helpAst;
-        for (int ast = 0; ast < Asteroiden.size(); ast++) {
-            if (Asteroiden.at(ast).hit == true) {
-            }
-            else {
-                helpAst.push_back(Asteroiden.at(ast));
-            }
-        }
-        Asteroiden.clear();
-        for (Asteorid ast : helpAst) {
-            Asteroiden.push_back(ast);
-        }
-
-        vector<Schuss> helpSt;
-        for (int st = 0; st < schuesse.size(); st++) {
-            if (schuesse.at(st).existent == false) {
-            }
-            else {
-                helpSt.push_back(schuesse.at(st));
-            }
-        }
-        schuesse.clear();
-        for (Schuss st : helpSt) {
-            schuesse.push_back(st);
-        }
-    }
-    void SchussBewegen() {
+   
+    void SchussBewegen()                                         // Sorgt dafür das die Schüsse sich von unten nach oben bewegen
+    {
         for (Schuss& st : schuesse) {
             st.SchussY -= schussSpeed;
         }
     }
-    void AsteroidenBewegen() {
+    void AsteroidenBewegen()                                    // Sorgt dafür das die Asteroiden sich von oben nach unten bewegen
+    {
         for (Asteorid& ast : Asteroiden)
         {
            ast.AsteroidY = ast.AsteroidY + SpaceSpeed; 
-           if ((RaumschiffX <= (ast.AsteroidX + (ast.AsteroidHoehe / 2)) && RaumschiffX >= (ast.AsteroidX - (ast.AsteroidHoehe / 2))) &&
-              (RaumschiffY <= (ast.AsteroidY - (ast.AsteroidBreite / 2)) && RaumschiffY >= (ast.AsteroidY + (ast.AsteroidBreite / 2))))
-            {
-                getroffen = true;
-                break;
-            }
-            
+           
         }
     }
-  
- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     void draw() override
     {
         if (spielAuswahl == 0) {
             SpaceHintergrund.draw(0, 0, 0, 1.0, 1.0);
-            Logo.draw(40, 0, 1, 1.0, 1.0);
-            auswahlSpaceInvaders.draw_text_rel("Spielen", 515, 500, 1, 0.5, 0.5, 1, 1, Gosu::Color::GREEN);                                                     // Menü beim Start des Spiels              
+            Logo.draw(40, 0, 1, 1.0, 1.0);                                                                          // Menü beim Start des Spiels  
+            auswahlSpaceInvaders.draw_text_rel("Spielen", 515, 500, 1, 0.5, 0.5, 1, 1, Gosu::Color::GREEN);                                                                 
         }
- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        if (spielAuswahl == 1) {
+        if (spielAuswahl == 1) 
+        {
             SpaceHintergrund.draw(0, 0, 0, 1.0, 1.0);
             string score = to_string(Score);
             string nothit = to_string(ScoreNichtZerstoert);
-            anzeigeScore.draw_text_rel("Score: " + score, 0, 0, 2, 0, 0, 1, 1, Gosu::Color::GREEN);
+            anzeigeScore.draw_text_rel("Score: " + score, 0, 0, 2, 0, 0, 1, 1, Gosu::Color::GREEN);                // Blendet den Score oben links ein
             
-
             if (Spielen) {
-                Raumschiff.draw_rot(RaumschiffX, RaumschiffY, 2, rotation, 0.5, 0.5,0.1, 0.1);
+                Raumschiff.draw_rot(RaumschiffX, RaumschiffY, 2, rotation, 0.5, 0.5,0.1, 0.1);                    // Blendet das Raumschiff ein
                 for (int ast = 0; ast < Asteroiden.size(); ast++) {
                     if (Asteroiden.at(ast).hit == false) {
-                        graphics().draw_rect(Asteroiden.at(ast).AsteroidX, Asteroiden.at(ast).AsteroidY, Asteroiden.at(ast).AsteroidBreite, Asteroiden.at(ast).AsteroidHoehe, Asteroiden.at(ast).AsteroidFarbe, Asteroiden.at(ast).AsteroidPos);
-                        Asteroid.draw((Asteroiden.at(ast).AsteroidX - 10), (Asteroiden.at(ast).AsteroidY-30), Asteroiden.at(ast).AsteroidPos,0.5, 0.5);
+                        graphics().draw_rect(Asteroiden.at(ast).AsteroidX, Asteroiden.at(ast).AsteroidY, Asteroiden.at(ast).AsteroidBreite, Asteroiden.at(ast).AsteroidHoehe, Asteroiden.at(ast).AsteroidFarbe, Asteroiden.at(ast).AsteroidPos);    // Blendet die Asteroiden ein
+                        Asteroid.draw((Asteroiden.at(ast).AsteroidX - 10), (Asteroiden.at(ast).AsteroidY-30), Asteroiden.at(ast).AsteroidPos,0.5, 0.5);         
 
                     }
                 }
                 for (int st = 0; st < schuesse.size(); st++) {
                     if (schuesse.at(st).existent) {
-                        graphics().draw_rect(schuesse.at(st).SchussX, schuesse.at(st).SchussY, schuesse.at(st).SchussBreite, schuesse.at(st).SchussHoehe, schuesse.at(st).SchussFarbe, schuesse.at(st).SchussHoehe);
+                        graphics().draw_rect(schuesse.at(st).SchussX, schuesse.at(st).SchussY, schuesse.at(st).SchussBreite, schuesse.at(st).SchussHoehe, schuesse.at(st).SchussFarbe, schuesse.at(st).SchussHoehe);   // Blendet die Schüsse ein
                     }
                 }
             }
             if (getroffen) {
                 graphics().draw_quad(400, 270, Gosu::Color::RED, 400, 330, Gosu::Color::RED, 600, 330, Gosu::Color::RED, 600, 270, Gosu::Color::RED, 1);
-                anzeigeRestart.draw_text_rel("Restart", 500, 300, 2, 0.5, 0.5, 2, 2, Gosu::Color::WHITE);
+                anzeigeRestart.draw_text_rel("Restart", 500, 300, 2, 0.5, 0.5, 2, 2, Gosu::Color::WHITE);                                                           // Zeigt den Restart-und Exit-Knopf wenn man getroffen wurde
                 exitgame.draw_text_rel("Exit", 970, 20, 2, 0.5, 0.5, 1, 1, Gosu::Color::RED);
             
             }
         }
        
     }
- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- // 
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     // Wird 60x pro Sekunde aufgerufen
     void update() override
     {
-        mausX = input().mouse_x();            //Position der Maus im Menü
+        mausX = input().mouse_x();            //Position der Maus
         mausY = input().mouse_y();
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
         if (spielAuswahl == 0) {
-            if (mausX >= 430 && mausX <= 600 && mausY >= 400 && mausY <= 600 && input().down(Gosu::Button::MS_LEFT)) {     
-                spielAuswahl = 1;                                                                                                // Menüauswahl
+            if (mausX >= 430 && mausX <= 600 && mausY >= 400 && mausY <= 600 && input().down(Gosu::Button::MS_LEFT))   // Abfrage ob Spielen im Menü gedrückt wurde
+            {   
+                spielAuswahl = 1;                                                                                                
                 Spielen = true;
-                restart_space();
+                restart_game();
                
             }
         }
- //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
        
-        if (spielAuswahl == 1) {
-            if (!getroffen) {
-                if (Score % speedIncreaseAt == 0 && speedIncrease) {
-                    SpaceSpeed = SpaceSpeed * SpeedIncreaseFaktor;
+        if (spielAuswahl == 1)
+        {
+            if (!getroffen)                                                                             // Alle 5 Punkte wird die Geschwindigkeit erhöht
+            {
+                if (Score % speedIncreaseAt == 0 && speedIncrease) {            
+                    SpaceSpeed = SpaceSpeed * SpeedIncreaseFaktor;          
                     speedIncrease = false;
                 }
                 AsteroidenAnzahl--;
@@ -214,36 +219,33 @@ public:
                 
                 if (input().down(Gosu::Button::KB_LEFT) || input().down(Gosu::Button::KB_A)) 
                 {
-                    RaumschiffX -= (SpaceSpeed + 2);
+                    RaumschiffX -= (SpaceSpeed + 2);                                                      // Steuerung des Raumschiffs nach links                                                
                 }
                 else if (input().down(Gosu::Button::KB_RIGHT) || input().down(Gosu::Button::KB_D)) 
                 {
-                    RaumschiffX += SpaceSpeed + 2;
+                    RaumschiffX += SpaceSpeed + 2;                                                        // Steuerung des Raumschiffs nach rechts                                                  
                 }
-                
                 if (AsteroidenAnzahl <= 1) {
-                    AsteroidenAnzahl = 30;
-                    erstelleAsteroid(Asteroiden);
-                    
-                       
+                    AsteroidenAnzahl = 20;                                                                // verlangsamt das Spawnen der Asteroiden
+                    erstelleAsteroid(Asteroiden);    
                 }
                 if (input().down(Gosu::Button::KB_SPACE)&& schussZahl>=10) {
-                    schuesse.push_back(erstelleSchuss(RaumschiffX, RaumschiffY));
+                    schuesse.push_back(erstelleSchuss(RaumschiffX, RaumschiffY));                         // lässt Schüsse beim betätigen der Leertaste spawnen
                     schussZahl = 0;
                 }
-                AstInRange(); 
+                FunktionenObjekte(); 
                 SchussBewegen();
                 AsteroidenBewegen();
             }
-            else if (mausX >= 410 && mausX <= 590 && mausY >= 270 && mausY <= 330 && input().down(Gosu::Button::MS_LEFT)) {    // Restart Knopf wurde betätigt
-                restart_space();  
+            else if (mausX >= 410 && mausX <= 590 && mausY >= 270 && mausY <= 330 && input().down(Gosu::Button::MS_LEFT)) {       // Restart Knopf wurde betätigt
+                restart_game();  
                 getroffen = false;
                 return;
             }
-            else if (mausX >= 900 && mausX <= 1000 && mausY >= 0 && mausY <= 100 && input().down(Gosu::Button::MS_LEFT)) {               //Spiel beenden
+            else if (mausX >= 900 && mausX <= 1000 && mausY >= 0 && mausY <= 100 && input().down(Gosu::Button::MS_LEFT)) {       //Spiel beenden
                 exit(3);
             }
-            if (RaumschiffX > 1000 || RaumschiffX< 0)              // Abfrage, ob man außerhalb den Bildschirms geflogen ist falls ja Game Over
+            if (RaumschiffX > 1000 || RaumschiffX< 0)                                                                            // Abfrage, ob man außerhalb den Bildschirms geflogen ist falls ja Game Over
             getroffen = true;
         }
     }
